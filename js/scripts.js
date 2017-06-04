5aa643ff8d6c73c6b271150b31222c6d5825e96a
Mick Sexton <lacrookedbeat@hotmail.com>
2017-06-03 21:52:51 -0700

    finished logic in js for triangle tester .... for now, logic isn't perfect

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,26 @@ $(document).ready(function() {
     var side1 = parseFloat($("input#side1").val());
     var side2 = parseFloat($("input#side2").val());
     var side3 = parseFloat($("input#side3").val());
+    var result;
 
-    alert(side1 + " " + side2 + " " + side3);
+    if (side1+side2 <= side3 || side2+side3 <= side1 || side1+side3 <= side2) {
+      result = "NOT a Triangle, bro";
+    }
+    else if (side1 === side2 && side1 === side3) {
+      result = "an Equilateral Triangle";
+    }
+    else if (side1 === side2 || side1 === side3 || side2 === side3) {
+        result = "an Isosceles Triangle";
+    }
+    else if (side1 != side2 && side1 != side3 && side2 != side3) {
+      result = "a Scalene Triangle";
+    }
+    else {
+      result = "Logic Error man!";
+    }
+
+    $("#result").text(result);
+    $(".result").show();
+    event.preventDefault();
   });
 });
